fd78341bcbbb47e27cabe187e7e8c466f8a4e969
mrfontana <kheartse@yahoo.com>
2022-07-14 22:05:12 -0700

    Making sure repo is good locally

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 	function getSessions(){
 		return new Promise(function(resolve, reject){
 		  var oReq = new XMLHttpRequest();
-		  oReq.onload = function (e) {
+		  oReq.onload = function (e) { 
 			resolve(e.target.response);
 		  };
 		  oReq.open('GET', 'sessions.json', true);
